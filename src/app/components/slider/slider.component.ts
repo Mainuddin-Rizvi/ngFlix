d9b06853c86166f61bb436d50550d8ee6fa345bf
Mainuddin Rizvi <web97development@gmail.com>
2023-10-21 05:27:19 +0530

    Getting popular movies

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit{
+  constructor(private http:HttpClient){}
 
+  ngOnInit(): void {  //life cycle hook of components
+    this.http.get(
+      'https://api.themoviedb.org/3/movie/popular?api_key=606bb1999289eb80238ba12395b4496f'
+      ).subscribe(data=>{
+        console.log(data);
+
+      })
+  }
 }
