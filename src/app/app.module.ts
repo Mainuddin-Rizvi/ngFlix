1a3e9ee99a838db92f5961432226653b5ba97182
Mainuddin Rizvi <web97development@gmail.com>
2023-10-21 19:16:05 +0530

    show item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { SliderComponent } from './components/slider/slider.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MoviesService } from './services/movies.service';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { BannerComponent } from './components/banner/banner.component'
+import { BannerComponent } from './components/banner/banner.component';
+import { ShowItemComponent } from './components/show-item/show-item.component'
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { BannerComponent } from './components/banner/banner.component'
     MovieListComponent,
     SliderComponent,
     BannerComponent,
+    ShowItemComponent,
   ],
   imports: [
     BrowserModule,
