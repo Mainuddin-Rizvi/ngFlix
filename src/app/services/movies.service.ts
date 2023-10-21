715ba717df867f0e61832a274820a0d4939dc672
Mainuddin Rizvi <web97development@gmail.com>
2023-10-21 13:01:45 +0530

    loop over the movies ngFor to create slide

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -7,7 +7,7 @@ export class MoviesService {
   constructor(private http:HttpClient) {}
 
   getPopularMovies(){
-    return this.http.get(
+    return this.http.get<any>(
       'https://api.themoviedb.org/3/movie/popular?api_key=606bb1999289eb80238ba12395b4496f'
       );
   }
