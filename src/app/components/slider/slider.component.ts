fb54ae85d4977aee8169c0f404875a577c4cd4e5
Mainuddin Rizvi <web97development@gmail.com>
2023-10-21 05:33:28 +0530

    show the data in front end

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -7,14 +7,19 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit{
+  movies : any;
+
   constructor(private http:HttpClient){}
 
-  ngOnInit(): void {  //life cycle hook of components
+  ngOnInit(){
+   this.getPopularMovies();
+  }
+
+  getPopularMovies(){
     this.http.get(
       'https://api.themoviedb.org/3/movie/popular?api_key=606bb1999289eb80238ba12395b4496f'
       ).subscribe(data=>{
-        console.log(data);
-
+        this.movies = data;
       })
   }
 }
