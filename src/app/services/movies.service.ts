bd42c20e7da803d1a0b4d0c643f65c3636d9bbb3
Mainuddin Rizvi <web97development@gmail.com>
2023-10-21 13:11:11 +0530

    Define Movie Model

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,3 +1,4 @@
+import { MoviesDto } from './../types/movie';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -7,7 +8,7 @@ export class MoviesService {
   constructor(private http:HttpClient) {}
 
   getPopularMovies(){
-    return this.http.get<any>(
+    return this.http.get<MoviesDto>(
       'https://api.themoviedb.org/3/movie/popular?api_key=606bb1999289eb80238ba12395b4496f'
       );
   }
