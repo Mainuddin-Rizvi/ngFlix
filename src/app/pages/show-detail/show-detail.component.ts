6cf5d37fae13edb973a42d2eb118a5f80a870fb4
Mainuddin Rizvi <web97development@gmail.com>
2023-10-23 10:34:08 +0530

    Reading route parameter

--- src/app/pages/show-detail/show-detail.component.ts
+++ src/app/pages/show-detail/show-detail.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-show-detail',
   templateUrl: './show-detail.component.html',
   styleUrls: ['./show-detail.component.scss']
 })
-export class ShowDetailComponent {
+export class ShowDetailComponent implements OnInit{
+
+  showId = 0;
+  constructor(private router: ActivatedRoute){}
+  ngOnInit(): void {
+    // this.router.params.subscribe(params => {
+    //   console.log(params);
+    //   this.showId = params['id'];
+    // });
+
+    this.showId = this.router.snapshot.params['id']
+  }
+
 
 }
