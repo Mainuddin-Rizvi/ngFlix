d17b2c9a411c92b0fe8627274b647598e3cb1c5a
Mainuddin Rizvi <web97development@gmail.com>
2023-10-23 11:09:45 +0530

    get movie by id

--- src/app/pages/show-detail/show-detail.component.ts
+++ src/app/pages/show-detail/show-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { MoviesService } from 'src/app/services/movies.service';
+import { Movie } from 'src/app/types/movie';
 
 @Component({
   selector: 'app-show-detail',
@@ -8,15 +11,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ShowDetailComponent implements OnInit{
 
-  showId = 0;
-  constructor(private router: ActivatedRoute){}
+  showId = '';
+
+  show$: Observable<Movie> | null = null;
+  constructor(private router: ActivatedRoute,private moviesService : MoviesService){}
   ngOnInit(): void {
     // this.router.params.subscribe(params => {
     //   console.log(params);
     //   this.showId = params['id'];
     // });
 
-    this.showId = this.router.snapshot.params['id']
+    this.showId = this.router.snapshot.params['id'];
+    this.show$ = this.moviesService.getMovieById(this.showId);
   }
 
 
