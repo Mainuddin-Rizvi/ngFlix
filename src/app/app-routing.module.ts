b04d80ce709824270cd58c2d0f80a5931baa1298
Mainuddin Rizvi <web97development@gmail.com>
2023-10-28 11:20:41 +0530

    routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,12 @@ import { ShowsListComponent } from './pages/shows-list/shows-list.component';
 import { GenresComponent } from './pages/genres/genres.component';
 
 const routes: Routes = [
-  {path: '',component:HomeComponent},
-  {path: 'list/:type',component:ShowsListComponent},
-  {path: 'detail/:id/:type',component:ShowDetailComponent},
-  {path: 'genres',component:GenresComponent},
-  {path: 'genres/:genreId',component:GenresComponent}
+
+  { path: '', component: HomeComponent },
+  { path: 'list/:type', component: ShowsListComponent },
+  { path: 'detail/:id', component: ShowDetailComponent },
+  { path: 'genres', component: GenresComponent },
+  { path: 'genres/:genreId', component: GenresComponent },
 
 
 ];
